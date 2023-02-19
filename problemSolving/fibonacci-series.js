189fce579ba369277f201e5b141def440526a14f
Sajjad Ali <sajjad.ali@arpatech.com>
2023-02-19 11:27:55 +0500

    Fixed series

--- problemSolving/fibonacci-series.js
+++ problemSolving/fibonacci-series.js
@@ -2,14 +2,16 @@ function fibonacciSeries(n) {
   let firstNumber = 1;
   let secondNumber = 0;
   let nextNumber = 0;
+  let arr = []
   for (let i = 0; i < n; i++) {
     nextNumber = firstNumber + secondNumber;
     firstNumber = secondNumber;
     secondNumber = nextNumber;
+    arr.push(secondNumber)
   }
-  return secondNumber;
+  return arr;
 }
 
 const number = 7;
-console.log(fibonacciSeries(number));
+console.log(fibonacciSeries(number).join(' '));
 // 1 1 2 3 5 8 13 21 34
