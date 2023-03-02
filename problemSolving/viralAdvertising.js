f8eaad3a782e8ced4c29f9865548a0af3975e07f
Sajjad Ali <sajjad.ali@arpatech.com>
2023-03-02 16:39:17 +0500

    Resolved haker rank's viralAdvertising problem

---
+++ problemSolving/viralAdvertising.js
@@ -0,0 +1,14 @@
+function beautifulDays(n) {
+  let shared = 5;
+  let sharedWith = 3;
+  let commulative = 0;
+  for (var i=0; i<n; i++) {
+    console.log(i)
+   let likedBy = Math.floor(shared/2)
+   commulative += likedBy;
+   shared = likedBy * sharedWith;
+  }
+  return commulative;
+}
+
+console.log(beautifulDays(5));
