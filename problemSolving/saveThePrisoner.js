5869bc0401f54bd451e16a3d53ee46541fb2a798
Sajjad Ali <sajjad.ali@arpatech.com>
2023-03-14 15:30:42 +0500

    Solved

--- problemSolving/saveThePrisoner.js
+++ problemSolving/saveThePrisoner.js
@@ -1,23 +1,8 @@
 function saveThePrisoner(n, m, s) {
-  const max = 1000000000;
-  if ( n > max || m > max || n < 0 || n > 100) return 0
-  let counter = 1;
-  let startFrom;
-  while (counter <= m) {
-    if (counter === 1) startFrom = s;
-    else startFrom += 1;
-
-    if (startFrom === n) {
-      if (counter != m) {
-        startFrom = 0;
-      }
-    }
-    counter += 1;
-  }
-  return startFrom;
+  return (m - 1  + s - 1 ) % n + 1
 }
-const n = 7,
-  m = 19,
-  s = 2;
- //const n = 3, m = 7, s = 3;
+// const n = 7, m = 19, s = 2;
+const n = 5, m = 2, s = 1;
+// 3
+// const n = 3, m = 7, s = 3;
 console.log(saveThePrisoner(n, m, s));
